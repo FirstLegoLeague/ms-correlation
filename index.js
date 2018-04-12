9137f882fd6791bcee07be68ce7e172bd948b680
Roy Shmeuli <roysh9@gmail.com>
2018-04-12 16:09:09 +0300

    Adding the code

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 'use strict'
+
+const { correlationMiddleware } = require('./lib/middleware')
+const { correlateSession, getCorrelationId, getAuthenticationData } = require('./lib/session')
+
+module.exports = {
+  correlationMiddleware,
+  correlateSession,
+  getCorrelationId,
+  getAuthenticationData
+}
