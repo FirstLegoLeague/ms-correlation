789c20a315a8fd5004ed850d6952c17d6ca45884
Roy Shmeuli <roysh9@gmail.com>
2018-07-12 12:51:44 +0300

    Add an mhub listener

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 const { correlationMiddleware } = require('./lib/middleware')
 const { correlateSession, getCorrelationId, getAuthenticationData } = require('./lib/session')
+const { mhubListenerWrapper } = require('./lib/mhub')
 
 module.exports = {
   correlationMiddleware,
   correlateSession,
   getCorrelationId,
-  getAuthenticationData
+  getAuthenticationData,
+  mhubListenerWrapper
 }
